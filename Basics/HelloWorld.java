43d507a23104ea8a88ea05c5bb2048754c91a3b1
Awais <awaisarbab676@gmail.com>
2025-10-07 21:09:51 +0500

    Third commit

--- Basics/HelloWorld.java
+++ Basics/HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld{
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("it's the second commit");
+        System.out.println("It's third comment");
     }
 }
